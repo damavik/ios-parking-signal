c89268140dabbc2b56c151d02c6778a24b745ea0
Vital Vinahradau <vital@fiverun.com>
2016-05-12 22:41:41 +0300

    Add dev certificate.

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -17,6 +17,8 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./scripts/certs/debug.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./scripts/certs/debug.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 
 
 # Put the provisioning profile in place
